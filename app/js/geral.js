590eaecb007373923dc53f029ff09e2984547075
leonordd <leonorddomingues3@gmail.com>
2024-05-18 17:37:47 +0100

    slider theme page

---
+++ app/js/geral.js
@@ -0,0 +1,10 @@
+const mySlider = document.getElementById("my-slider");
+const sliderValue = document.getElementById("slider-value");
+
+function slider(){
+    valPercent = (mySlider.value / mySlider.max)*100;
+    sliderValue.textContent = mySlider.value;
+    mySlider.style.background = `linear-gradient(to right, #0024B5 ${valPercent}%, #DCE3FF ${valPercent}%)`;
+}
+
+slider();
