ac272c2679957ecd405899c61bff129995089ba6
xana <acordeiro2002@gmail.com>
2024-06-06 15:14:25 +0100

    update arduino part

--- light_box_subscriber/light_box_subscriber.ino
+++ light_box_subscriber/light_box_subscriber.ino
@@ -3,34 +3,18 @@
 #include <FastLED.h>
 #include "esp_wpa2.h"
 
-#define LED_PIN  33
-#define NUM_LEDS    50
+// LEDS
+#define FASTLED_ALLOW_INTERRUPTS 0
+#include <FastLED.h>
+
+#define LED_PIN            33
+#define NUM_LEDS            30
+#define MAX_POWER_MILLIAMPS 500
 #define BRIGHTNESS  64
-#define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
+#define LED_TYPE            WS2812B
+#define COLOR_ORDER         GRB
 CRGB leds[NUM_LEDS];
-
 #define UPDATES_PER_SECOND 100
-#define EAP_ANONYMOUS_IDENTITY ""
-#define EAP_IDENTITY "" 
-#define EAP_PASSWORD "" 
-#define EAP_USERNAME ""
-
-bool led_pattern_1 = false;
-
-const char ssid[] = "";
-const char pass[] = "";
-
-
-/*const char ssid[] = "eduroam";
-const char pass[] = "";*/
-
-const char MQTT_BROKER_ADRRESS[] = "test.mosquitto.org";
-const int MQTT_PORT = 1883;
-
-
-WiFiClient net;
-MQTTClient client = MQTTClient(256);
 
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
@@ -38,14 +22,43 @@ TBlendType    currentBlending;
 extern CRGBPalette16 myRedWhiteBluePalette;
 extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 
-void setup() {
-  Serial.begin(9600);
+// WIFI
+#define EAP_ANONYMOUS_IDENTITY "anonymous@uc.pt"
+#define EAP_IDENTITY "#######@student.uc.pt" 
+#define EAP_PASSWORD "#######" 
+#define EAP_USERNAME "#######@student.uc.pt"
 
+const char ssid[] = "";
+const char pass[] = "";
+// const char ssid[] = "eduroam";
+
+bool led_pattern_1 = false;
+bool led_pattern_2 = false;
+
+const char MQTT_BROKER_ADRRESS[] = "test.mosquitto.org";
+const int MQTT_PORT = 1883;
+
+bool on_payload = false;
+
+WiFiClient net;
+MQTTClient client = MQTTClient(256);
+
+void setup() {
+  
+  // led
+  delay(3000); 
+  FastLED.addLeds<LED_TYPE,LED_PIN,COLOR_ORDER>(leds, NUM_LEDS)
+        .setCorrection( TypicalLEDStrip );
+  FastLED.setMaxPowerInVoltsAndMilliamps( 5, MAX_POWER_MILLIAMPS);
+
+  // pattern 2
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
   
   currentPalette = RainbowColors_p;
   currentBlending = LINEARBLEND;
+
+  Serial.begin(9600);
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -69,11 +82,19 @@ void setup() {
 void loop() {
   client.loop();
 
-  if (led_pattern_1) {
+  Serial.println(on_payload);
+  if (!on_payload) FastLED.clear(true);
+
+  if (led_pattern_1 && on_payload) {
+    pacifica_loop();
+    FastLED.show();
+  }
+
+  if (led_pattern_2 && on_payload) {
     ChangePalettePeriodically();
     
     static uint8_t startIndex = 0;
-    startIndex = startIndex + 1; /* motion speed */
+    startIndex = startIndex + 1;
     
     FillLEDsFromPaletteColors( startIndex);
     
@@ -89,7 +110,7 @@ void connectToMQTT() {
 
   Serial.println("Connecting to MQTT broker...");
 
-  while (!client.connect("arduinoESP32XPTO123"/*, "public", "public"*/)) {
+  while (!client.connect("arduinoESP32XPTO123")) {
     Serial.print(".");
     delay(100);
   }
@@ -102,21 +123,13 @@ void connectToMQTT() {
   else
     Serial.println("Failed to subscribe to the topic: sensor/toy1");
   
-  /*
+  
   // toy 2
   if (client.subscribe("sensor/toy2"))
     Serial.println("Subscribed to the topic: sensor/toy2");
   else
     Serial.println("Failed to subscribe to the topic: sensor/toy2");
-  
-  // toy 3
-  if (client.subscribe("sensor/toy3"))
-    Serial.println("Subscribed to the topic: sensor/toy3");
-  else
-    Serial.println("Failed to subscribe to the topic: sensor/toy3");
-
-  */
-  
+    
   Serial.println("Connected to broker!");
 }
 
@@ -126,23 +139,29 @@ void messageReceived(String &topic, String &payload) {
   Serial.print("- payload: ");
   Serial.println(payload);
 
-  char command = payload[0];
   
+  if (payload == "1") on_payload = true;
+  else on_payload = false;
+
   if (topic == "sensor/toy1") {
     led_pattern_1 = true;
+    led_pattern_2 = false;
+    Serial.println("received from toy 1");
   } else if (topic == "sensor/toy2") {
     led_pattern_1 = false; 
+    led_pattern_2 = true;
     Serial.println("received from toy 2");
-  } else if (topic == "sensor/toy3") {
-    led_pattern_1 = false; 
-    Serial.println("received from toy 3");
-  } else {
+  }
+  else {
     Serial.println("not subscribed to this topic");
   }
 
 }
 
-// Note: The following LED effect can be found in one of the examples included in the FastLED Library
+
+
+//////////////////////////////////////////////////////////////////////
+// "Color Palette" LED pattern from the FAST LED Library Examples: ColorPalette.ino
 
 void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
@@ -153,7 +172,6 @@ void FillLEDsFromPaletteColors( uint8_t colorIndex)
         colorIndex += 3;
     }
 }
-
 
 void ChangePalettePeriodically()
 {
@@ -176,7 +194,6 @@ void ChangePalettePeriodically()
     }
 }
 
-
 void SetupTotallyRandomPalette()
 {
     for( int i = 0; i < 16; ++i) {
@@ -186,16 +203,13 @@ void SetupTotallyRandomPalette()
 
 void SetupBlackAndWhiteStripedPalette()
 {
-    
     fill_solid( currentPalette, 16, CRGB::Black);
-    
     currentPalette[0] = CRGB::White;
     currentPalette[4] = CRGB::White;
     currentPalette[8] = CRGB::White;
     currentPalette[12] = CRGB::White;
     
 }
-
 
 void SetupPurpleAndGreenPalette()
 {
@@ -214,7 +228,7 @@ void SetupPurpleAndGreenPalette()
 const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
 {
     CRGB::Red,
-    CRGB::Gray, 
+    CRGB::Gray, // 'white' is too bright compared to red and blue
     CRGB::Blue,
     CRGB::Black,
     
@@ -232,3 +246,102 @@ const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
     CRGB::Black,
     CRGB::Black
 };
+
+//////////////////////////////////////////////////////////////////////////
+
+//  "Pacifica" LED pattern from the Fast LED Library
+//  Gentle, blue-green ocean waves.
+//  December 2019, Mark Kriegsman and Mary Corey March.
+//  For Dan.
+
+CRGBPalette16 pacifica_palette_1 = 
+    { 0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117, 
+      0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x14554B, 0x28AA50 };
+CRGBPalette16 pacifica_palette_2 = 
+    { 0x000507, 0x000409, 0x00030B, 0x00030D, 0x000210, 0x000212, 0x000114, 0x000117, 
+      0x000019, 0x00001C, 0x000026, 0x000031, 0x00003B, 0x000046, 0x0C5F52, 0x19BE5F };
+CRGBPalette16 pacifica_palette_3 = 
+    { 0x000208, 0x00030E, 0x000514, 0x00061A, 0x000820, 0x000927, 0x000B2D, 0x000C33, 
+      0x000E39, 0x001040, 0x001450, 0x001860, 0x001C70, 0x002080, 0x1040BF, 0x2060FF };
+
+
+void pacifica_loop()
+{
+  // Increment the four "color index start" counters, one for each wave layer.
+  // Each is incremented at a different speed, and the speeds vary over time.
+  static uint16_t sCIStart1, sCIStart2, sCIStart3, sCIStart4;
+  static uint32_t sLastms = 0;
+  uint32_t ms = GET_MILLIS();
+  uint32_t deltams = ms - sLastms;
+  sLastms = ms;
+  uint16_t speedfactor1 = beatsin16(3, 179, 269);
+  uint16_t speedfactor2 = beatsin16(4, 179, 269);
+  uint32_t deltams1 = (deltams * speedfactor1) / 256;
+  uint32_t deltams2 = (deltams * speedfactor2) / 256;
+  uint32_t deltams21 = (deltams1 + deltams2) / 2;
+  sCIStart1 += (deltams1 * beatsin88(1011,10,13));
+  sCIStart2 -= (deltams21 * beatsin88(777,8,11));
+  sCIStart3 -= (deltams1 * beatsin88(501,5,7));
+  sCIStart4 -= (deltams2 * beatsin88(257,4,6));
+
+  // Clear out the LED array to a dim background blue-green
+  fill_solid( leds, NUM_LEDS, CRGB( 2, 6, 10));
+
+  // Render each of four layers, with different scales and speeds, that vary over time
+  pacifica_one_layer( pacifica_palette_1, sCIStart1, beatsin16( 3, 11 * 256, 14 * 256), beatsin8( 10, 70, 130), 0-beat16( 301) );
+  pacifica_one_layer( pacifica_palette_2, sCIStart2, beatsin16( 4,  6 * 256,  9 * 256), beatsin8( 17, 40,  80), beat16( 401) );
+  pacifica_one_layer( pacifica_palette_3, sCIStart3, 6 * 256, beatsin8( 9, 10,38), 0-beat16(503));
+  pacifica_one_layer( pacifica_palette_3, sCIStart4, 5 * 256, beatsin8( 8, 10,28), beat16(601));
+
+  // Add brighter 'whitecaps' where the waves lines up more
+  pacifica_add_whitecaps();
+
+  // Deepen the blues and greens a bit
+  pacifica_deepen_colors();
+}
+
+// Add one layer of waves into the led array
+void pacifica_one_layer( CRGBPalette16& p, uint16_t cistart, uint16_t wavescale, uint8_t bri, uint16_t ioff)
+{
+  uint16_t ci = cistart;
+  uint16_t waveangle = ioff;
+  uint16_t wavescale_half = (wavescale / 2) + 20;
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    waveangle += 250;
+    uint16_t s16 = sin16( waveangle ) + 32768;
+    uint16_t cs = scale16( s16 , wavescale_half ) + wavescale_half;
+    ci += cs;
+    uint16_t sindex16 = sin16( ci) + 32768;
+    uint8_t sindex8 = scale16( sindex16, 240);
+    CRGB c = ColorFromPalette( p, sindex8, bri, LINEARBLEND);
+    leds[i] += c;
+  }
+}
+
+// Add extra 'white' to areas where the four layers of light have lined up brightly
+void pacifica_add_whitecaps()
+{
+  uint8_t basethreshold = beatsin8( 9, 55, 65);
+  uint8_t wave = beat8( 7 );
+  
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    uint8_t threshold = scale8( sin8( wave), 20) + basethreshold;
+    wave += 7;
+    uint8_t l = leds[i].getAverageLight();
+    if( l > threshold) {
+      uint8_t overage = l - threshold;
+      uint8_t overage2 = qadd8( overage, overage);
+      leds[i] += CRGB( overage, overage2, qadd8( overage2, overage2));
+    }
+  }
+}
+
+// Deepen the blues and greens
+void pacifica_deepen_colors()
+{
+  for( uint16_t i = 0; i < NUM_LEDS; i++) {
+    leds[i].blue = scale8( leds[i].blue,  145); 
+    leds[i].green= scale8( leds[i].green, 200); 
+    leds[i] |= CRGB( 2, 5, 7);
+  }
+}
